84bf94dd5d3d14927b5c495c89bc51098a432f74
Denny George <denny@quadrant2.us>
2018-09-21 16:34:24 -0400

    basic react app setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import Feed from './components/Feed';
+import Login from './components/Login';
+import MapView from './components/MapView';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/feed" component={Feed} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/map-view" component={MapView} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MapView.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MapView extends Component{
+	render(){
+		return(
+			<h1> MapView </h1>
+		)
+	}
+}
+
+export default MapView
